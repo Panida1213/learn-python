0afb8389abcb63076ccbc3a02f3e68ef831e9fc5
Panida Pholdongnok <Boombieswiftie@gmail.com>
2025-10-05 16:46:44 +0700

    Test for loop

--- for loop.py
+++ for loop.py
@@ -1,3 +1,15 @@
 #ทดสอบเขียน for loop ใช้ในกรณีรู้จำนวนรอบ
-for counter in range(1,6,1): #ถ้าเขียน(5)จะทำ 5 รอบ ถ้าเขียน(1,6) จะทำ 1-5 
+for counter in range(1,6,): #ถ้าเขียน(1,6) จะทำ 1-5 
+    print(counter)
+
+for counter in range(7): #ถ้าเขียนใน range 7 หมายถึง นับตั้งแต่ 1-6
+    print(counter)
+
+for counter in range(2,6): #ถ้าเขียน range 2,6 หมายถึงเริ่มตั้นนับที่ 2 ถึง 5 เพราะ 6 คือหยุดนับ
+    print(counter)
+
+for counter in range(10,0,-1): #หมายถึงให้นับลงไปจาก 10 ทีละ -1
+    print(counter)
+
+for counter in range(1,10,2):#หมายถึงให้นับเริ่มจาก 1 เพิ่มขึ้นทีละ 2 จนภึง 10
     print(counter)
